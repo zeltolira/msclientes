4d9d3334eecbf045c6ecb45933d8828de2fd78ff
unknown <zelto@hotmail.com>
2023-12-28 06:51:47 -0300

    adiciona dependencia jpa no pom e cria cliente controller, service e repository

--- src/main/java/com/lira/msclientes/application/ClienteController.java
+++ src/main/java/com/lira/msclientes/application/ClienteController.java
@@ -1,2 +1,15 @@
-package com.lira.msclientes.application;public class ClienteController {
+package com.lira.msclientes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clientes")
+public class ClienteController {
+
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
 }
